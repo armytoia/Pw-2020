7c06f1aefcf8fd521ae03740eeb1f6f902eb3427
armytoia <vitoriaacaldeirinha@gmail.com>
2020-03-09 09:24:19 -0300

    segunda_versao

---
+++ aula04.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang=pt-br dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Teste CSS e HTML</title>
+    <link href="https://fonts.googleapis.com/css?family=Acme|Bellota&display=swap" rel="stylesheet">
+    <link rel = "stylesheet" href ="aula.css" >
+  </head>
+  <body>
+    <div class = "site">
+      <div class = "topo">
+        <h2>Topo do site</h2>
+        </div>
+        <div class = "conteudo">
+          Conteudo do site
+        </div>
+        <div class = rodape>
+          Desenvolvido por Vitória
+        </div>
+      </div>
+  </body>
+</html>
