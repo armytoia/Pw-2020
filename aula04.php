7216d50908fdc9bde5efc3d1b26c0386464b50fc
armytoia <vitoriaacaldeirinha@gmail.com>
2020-03-16 09:43:49 -0300

    terceira_versao

--- aula04.php
+++ aula04.php
@@ -12,8 +12,14 @@
         <h2>Topo do site</h2>
         </div>
         <div class = "conteudo">
-          Conteudo do site
-        </div>
+          <h2>Noticias<h2>
+          <div class="noticias">
+            <h3>Últimas notícias de coronavírus de 1º de março<h3>
+            <img src = "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSUJJPz-mQcz6vSxfYBhXE8AU-jevRk_gqb-IzPuOwpXL-dpAty">
+            <p>Na noite deste domingo, a China registrou 202 novos casos de coronavírus, elevando o número total para 80.026. O número de novas mortes foi de 42, totalizando 2.912 desde o início do surto.</p>
+            <p>Mais cedo, a Inglaterra anunciou 13 novos casos registrados, levando o total para 36. Ainda na Europa, o museu mais famoso de Paris, o Louvre, foi fechado na parte da manhã para que funcionários participassem de uma reunião sobre o covid-19.</p>
+            <p>Para aliviar os impactos do surto, o ministro da Economia, Roberto Gualtieri, anunciou neste domingo que vai injetar 3,6 bilhões de euros na economia italiana, a partir de estímulos como créditos tributários, reduções de impostos e financiamentos para a saúde pública.</p>
+          </div>
         <div class = rodape>
           Desenvolvido por Vitória
         </div>
---
+++ aula05.php
@@ -0,0 +1,75 @@
+  <!DOCTYPE html>
+  <html lang=pt-br dir="ltr">
+    <head>
+      <meta charset="utf-8">
+      <title>Teste CSS e HTML</title>
+      <link href="https://fonts.googleapis.com/css?family=Acme|Bellota&display=swap" rel="stylesheet">
+      <link href="https://fonts.googleapis.com/css?family=Kaushan+Script|Pacifico|Permanent+Marker&display=swap" rel="stylesheet">
+      <link rel = "stylesheet" href ="aula05.css" >
+    </head>
+    <body>
+      <div class = "site">
+        <div class = "topo">
+          <h2>Topo do site</h2>
+          <div class="menu">
+            <ul>
+              <li><a href= "#">início</a></li>
+              <li><a href= "#">cadastro</a></li>
+              <li><a href= "#">contato</a></li>
+              <li><a href= "#">ajuda</a></li>
+              <li><a href= "#">Login</a></li>
+            </ul>
+          </div>
+          </div>
+          <div class = "conteudo">
+            <h3>Criação de Botões</h3>
+              <a href="#" class = "botao botao-azul">Botão Azul</a>
+              <a href="#" class = "botao botao-vermelho">Botão Vermelho</a>
+              <a href="#" class = "botao botao-rosa">Botão Rosa</a>
+              <div class="noticias">
+                <h3>Últimas notícias de coronavírus de 1º de março<h3>
+                <img src = "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSUJJPz-mQcz6vSxfYBhXE8AU-jevRk_gqb-IzPuOwpXL-dpAty">
+                <p>Na noite deste domingo, a China registrou 202 novos casos de coronavírus, elevando o número total para 80.026. O número de novas mortes foi de 42, totalizando 2.912 desde o início do surto.</p>
+                <p>Mais cedo, a Inglaterra anunciou 13 novos casos registrados, levando o total para 36. Ainda na Europa, o museu mais famoso de Paris, o Louvre, foi fechado na parte da manhã para que funcionários participassem de uma reunião sobre o covid-19.</p>
+                <p>Para aliviar os impactos do surto, o ministro da Economia, Roberto Gualtieri, anunciou neste domingo que vai injetar 3,6 bilhões de euros na economia italiana, a partir de estímulos como créditos tributários, reduções de impostos e financiamentos para a saúde pública.</p>
+              </div>
+              <div class="noticias">
+                <h3>Cai para 11% o índice de alunos que aprendem o esperado em matemática</h3>
+
+                <p>Dados da Prova Brasil 2013 compilados pelo Portal QEdu mostram que o índice de alunos de escolas públicas que terminam o ensino fundamental com nível de aprendizado considerado adequado em matemática foi de 11,2% para os estudantes do 9º ano, índice inferior ao registrado na prova anterior, de 2011, quando a média foi de quase 12%. Já em português, com ênfase em leitura, a prova apresentou uma melhora de 22,2% para 23,6% no índice de alunos com aprendizado adequado.</p>
+                <p>Nas avaliações para o final do primeiro ciclo do ensino fundamental, no 5º ano, também houve melhora em português (de 36,2% em 2011, para 39,8% em 2013) e matemática (de 32,4% para 34,7%).</p>
+                <h3>Índices de proeficiência dos alunos de 5° e 9° ano do Ensino Fundamental na prova Brasil</h3>
+
+                  <table style="width:80%">
+                    <tr>
+                      <th>Nível</th>
+                      <th>Proeficiência</th>
+                    </tr>
+                    <tr>
+                      <td>Abaixo do nível 1 (- de 200 pts)</td>
+                      <td>Insuficiente</td>
+                    </tr>
+                    <tr>
+                      <td>Nível 4(275-300 pts)</td>
+                      <td>Básico</td>
+                    </tr>
+                    <tr>
+                      <td>Nível 9(+ de 400 pts)</td>
+                      <td>Avançado</td>
+                    </tr>
+
+                  </table>
+              </div>
+              <div class="noticias">
+                <h3>Homem armado mantém reféns em shopping nas Filipinas</h3>
+                <img width = "400" src= "https://s2.glbimg.com/apGb4jgS5tcKEmXjyWAcGQvQJxc=/0x0:1920x1080/1000x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_59edd422c0c84a879bd37670ae4f538a/internal_photos/bs/2020/Z/P/OmV8jGR0Cb7J3WmDlBGg/manila.jpg">
+                <p>Um homem armado abriu fogo dentro de um shopping e mantém reféns dentro do estabelecimento em uma área nobre de Manila, na capital das Filipinas.</p>
+                <p>O prefeito de San Juan, área onde ocorreu o ataque, disse à imprensa que aproximadamente 30 pessoas estão detidas pelo criminoso. Ele disse também que o atirador é um ex-guarda de segurança demitido. Ao menos uma das pessoas baleadas foi socorrida e levada para um hospital próximo. Não há informação sobre mortos.</p>
+              </div>
+            </div>
+          <div class = rodape>
+            Desenvolvido por Vitória
+          </div>
+        </div>
+    </body>
+  </html>
